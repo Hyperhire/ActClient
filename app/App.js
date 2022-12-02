13c704d895ad1e0461bc9eda81ba856bccb951dc
Lucas-Je <lucas@hyperhire.in>
2022-12-03 06:56:34 +0900

    init react native

---
+++ app/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { SafeAreaView, StatusBar, Text, View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+import WebView from 'react-native-webview';
+import styled from 'styled-components/native';
+
+const FullScreenContainer = styled.View`
+  flex: 1;
+  background-color: #fdd22c;
+`;
+
+const ContentContainer = styled(SafeAreaView)`
+  flex: 1;
+`;
+
+const App = () => {
+  useEffect(() => {
+    console.log('SplashScreen');
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <FullScreenContainer>
+      <StatusBar backgroundColor={'#fdd22c'} />
+      <ContentContainer>
+        <WebView source={{ uri: 'https://www.naver.com/' }} />
+      </ContentContainer>
+    </FullScreenContainer>
+  );
+};
+
+export default App;
