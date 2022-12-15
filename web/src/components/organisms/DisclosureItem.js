1696adf9ad620af2754d95f4e74d499b5b0b59bd
conan-kim <conan.kim@hyperhire.in>
2022-12-15 22:24:57 +0900

    modified: ui changed and minor fix

--- web/src/components/organisms/DisclosureItem.js
+++ web/src/components/organisms/DisclosureItem.js
@@ -3,12 +3,12 @@ import dayjs from 'dayjs';
 
 const DisclosureItem = props => {
   const { item, clickHandler } = props;
-  const { title, description, updatedAt } = item;
+  const { title, updatedAt, org } = item;
   return (
     <div className="disclosure-item-wrapper link" onClick={() => clickHandler(item)}>
-      <div className="disclosure-item-title">{title}</div>
+      <div className="disclosure-item-title">{org.name}</div>
       <div className="disclosure-item-description-wrapper">
-        <div className="disclosure-item-description">{description}</div>
+        <div className="disclosure-item-description">{title}</div>
         <div className="disclosure-item-date">{dayjs(updatedAt).format('YYYY.MM.DD')}</div>
       </div>
     </div>
